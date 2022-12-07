10bdc754ff56972f9acfa9753daca4e317fd977d
mohammadrn <mr.nezamdust@gmail.com>
2022-12-07 13:14:54 +0330

    feat: the header implemented

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,15 @@
 import { FC } from "react";
 import { Link } from "react-router-dom";
-// import logo from "./../../assets/images/logo.webp";
+import logo from "./../../assets/images/logo.webp";
 import "./style.scss";
 
 const Header: FC = () => {
   return (
     <div className="main-header">
       <div className="container">
-        {/* <Link className="logo" to="/">
+        <Link className="logo" to="/">
           <img src={logo} alt="we are eco" />
-        </Link> */}
+        </Link>
         <div className="menu">
           <Link className="item" to="/">
             خانه
